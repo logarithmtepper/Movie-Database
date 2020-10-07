8d6fed3d79a722d858d6c1d7e2aaeff240f2df20
maikutaison <71348096+maikutaison@users.noreply.github.com>
2020-10-07 15:49:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = http.createServer(function (request, response) {
   console.log("URL: " + request.url);
 
   if(request.method === 'GET'){
+    
     if(request.url === '/' || request.url === '/login.html'){
       fs.readFile("login.html", function(err, data){
 				if(err){
@@ -20,8 +21,8 @@ const server = http.createServer(function (request, response) {
 				response.end();
 			});
     }
+
     else if(request.url === "/loginStyle.css"){
-			//read todo.js file and send it back
 			fs.readFile("loginStyle.css", function(err, data){
 				if(err){
 					response.statusCode = 500;
@@ -34,9 +35,38 @@ const server = http.createServer(function (request, response) {
 				response.write(data);
 				response.end();
 			});
-		//Add any more 'routes' and their handling code here
-		//e.g., GET requests for "/list", POST request to "/list"
 		}
+
+    else if(request.url === "/loginApp.js"){
+			fs.readFile("loginApp.js", function(err, data){
+				if(err){
+					response.statusCode = 500;
+					response.write("Server error.");
+					response.end();
+					return;
+				}
+				response.statusCode = 200;
+				response.setHeader("Content-Type", "application/javascript");
+				response.write(data);
+				response.end();
+			});
+		}
+
+    else if(request.url === "/signUp"){
+			fs.readFile("signUp.html", function(err, data){
+				if(err){
+					response.statusCode = 500;
+					response.write("Server error.");
+					response.end();
+					return;
+				}
+				response.statusCode = 200;
+				response.setHeader("Content-Type", "text/html");
+				response.write(data);
+				response.end();
+			});
+		}
+
     else{
 			response.statusCode = 404;
 			response.write("Unknwn resource.");
