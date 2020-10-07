a4eebeac1a9a42db86b589b0afba24521270691f
maikutaison <71348096+maikutaison@users.noreply.github.com>
2020-10-07 16:08:08 -0400

    Update loginApp.js

--- loginApp.js
+++ loginApp.js
@@ -1,7 +1,13 @@
 funtcion createAccount(){
+  console.log("im here");
   let request = new XMLHttpRequest();
 
-  request.open("GET", "/signUp", true);
+  request.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      console.log(request.responseText);
+    }
+  }
+  request.open("GET", "/signUp.html", true);
   request.setRequestHeader('Accept', 'text/html');
   request.send();
 }
