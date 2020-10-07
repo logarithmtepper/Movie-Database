a3ab2b580af9e65f6f6f05dfae658be6cfd75998
maikutaison <71348096+maikutaison@users.noreply.github.com>
2020-10-07 16:55:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,70 +1,48 @@
 const http = require('http');
-
 const fs = require('fs');
 
 const server = http.createServer(function (request, response) {
   console.log("URL: " + request.url);
 
+  function processRequest(filename, contentType){
+    fs.readFile(filename, function(err, data){
+      if(err){
+        response.statusCode = 500;
+        response.write("Server error.");
+        response.end();
+        return;
+      }
+      response.statusCode = 200;
+      response.setHeader("Content-Type", contentType);
+      response.write(data);
+      response.end();
+    });
+  }
+
   if(request.method === 'GET'){
 
     if(request.url === '/' || request.url === '/login.html'){
-      fs.readFile("login.html", function(err, data){
-				if(err){
-					response.statusCode = 500;
-					response.write("Server error.");
-					response.end();
-					return;
-				}
-				response.statusCode = 200;
-				response.setHeader("Content-Type", "text/html");
-				response.write(data);
-				response.end();
-			});
+      processRequest("login.html", "text/html")
     }
 
     else if(request.url === "/loginStyle.css"){
-			fs.readFile("loginStyle.css", function(err, data){
-				if(err){
-					response.statusCode = 500;
-					response.write("Server error.");
-					response.end();
-					return;
-				}
-				response.statusCode = 200;
-				response.setHeader("Content-Type", "text/css");
-				response.write(data);
-				response.end();
-			});
+			processRequest("loginStyle.css", "text/css")
 		}
 
     else if(request.url === "/loginApp.js"){
-			fs.readFile("loginApp.js", function(err, data){
-				if(err){
-					response.statusCode = 500;
-					response.write("Server error.");
-					response.end();
-					return;
-				}
-				response.statusCode = 200;
-				response.setHeader("Content-Type", "application/javascript");
-				response.write(data);
-				response.end();
-			});
+			processRequest("loginApp.js", "application/javascript")
 		}
 
     else if(request.url === "/signUp.html"){
-			fs.readFile("signUp.html", function(err, data){
-				if(err){
-					response.statusCode = 500;
-					response.write("Server error.");
-					response.end();
-					return;
-				}
-				response.statusCode = 200;
-				response.setHeader("Content-Type", "text/html");
-				response.write(data);
-				response.end();
-			});
+			processRequest("signUp.html", "text/html")
+		}
+
+    else if(request.url === "/signUpStyle.css"){
+			processRequest("signUpStyle.css", "text/css")
+		}
+
+    else if(request.url === "/signUpApp.js"){
+			processRequest("signUpApp.js", "application/javascript")
 		}
 
     else{
@@ -73,7 +51,7 @@ const server = http.createServer(function (request, response) {
 			response.end();
 		}
 	}else if(request.method === "POST"){
-		//any handling in here
+		//handling goes here
 		response.statusCode = 404;
 		response.write("Unknwn resource.");
 		response.end();
