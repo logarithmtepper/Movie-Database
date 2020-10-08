3960b7bd18a9e33d89baf2006be809f9cab9e84f
maikutaison <71348096+maikutaison@users.noreply.github.com>
2020-10-07 20:39:37 -0400

    Update ServerApp.js

--- ServerApp.js
+++ ServerApp.js
@@ -8,25 +8,6 @@ request.onreadystatechange = function(){
   }
 }
 
-request.open("GET", "/login.html", true);
+request.open("GET", "/login.pug", true);
 request.setRequestHeader('Accept', 'text/html');
 request.send();
-
-
-function createAccount(){
-  let request = new XMLHttpRequest();
-
-  request.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-      console.log(request.responseText);
-
-
-      body.innerHTML = request.responseText
-
-    }
-  }
-
-  request.open("GET", "/signUp.html", true);
-  request.setRequestHeader('Accept', 'text/html');
-  request.send();
-}
