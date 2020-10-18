84be027972f2152bd789f856e156b45b122cb16b
maikutaison <71348096+maikutaison@users.noreply.github.com>
2020-10-18 18:42:12 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-  const express = require('express');
+const express = require('express');
 const router = express.Router();
 //const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
