95b1e0805c74fa1b927365f3ef0fa3457000364c
Wenyu Zhang <ivoryzhangwy@gmail.com>
2020-10-23 14:20:50 -0400

    Delete index.js

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-//const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
-
-router.get('/login', function(req, res, next) {
-  res.render('login', { error: false });
-});
-
-module.exports = router;
