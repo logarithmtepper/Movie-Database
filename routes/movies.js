bd9afd68d53820d412eb9cb05e4470f370166415
maikutaison <71348096+maikutaison@users.noreply.github.com>
2020-10-27 16:29:32 -0400

    Reorganized

--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const fs = require('fs');
 let movieData = require("../movie-data-short.json");
 
 //for GET /home
