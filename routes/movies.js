03bbd6d8116feb70692fc7d0a9db973486d8af67
Wenyu Zhang <ivoryzhangwy@gmail.com>
2020-11-12 23:21:57 -0500

    minor change

--- routes/movies.js
+++ routes/movies.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Movie = require("../movieModel");
+const User = require("../userModel");
 
 start = 10000;
 //for GET /home
