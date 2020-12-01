5db52516ef017ebadfee709815dcb5092a74dc93
maikutaison <71348096+maikutaison@users.noreply.github.com>
2020-11-30 23:46:25 -0500

    Added Webscraper

--- routes/movies.js
+++ routes/movies.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require("node-fetch");
 const router = express.Router();
 const Movie = require("../models/movieModel");
 const Person = require("../models/personModel");
@@ -17,7 +18,7 @@ router.get('/add', function(req, res){
 	}
 	else if (req.user.contributing !== "y"){
 		res.redirect("/users/profile");
-	}	
+	}
 	else{
 		res.render('addMovie.pug',{
 			user:req.user
@@ -25,6 +26,19 @@ router.get('/add', function(req, res){
 	}
 });
 
+router.get('/addbyurl', function(req, res){
+	if (req.user === undefined){
+		res.redirect("/users/login");
+	}
+	else if (req.user.contributing !== "y"){
+		res.redirect("/users/profile");
+	}
+	else{
+		res.render('addMovieByUrl.pug',{
+			user:req.user
+		});
+	}
+});
 
 function addPersonToMovie(list,role,people, movie){
 	const movie_obj = {id:movie.id, name:movie.title}
@@ -73,9 +87,13 @@ function addPersonToMovie(list,role,people, movie){
 				});
 			}
 		})
-	
+
 	}
 }
+
+router.post('/addbyurl', function(req, res, next){
+
+})
 
 router.post('/add', function(req, res, next){
 	const title = req.body.mname;
@@ -105,8 +123,8 @@ router.post('/add', function(req, res, next){
 		res.render("addMovie",{
 			error:"This movie exists in the database"
 		});
-	  } 
-	  else{	
+	  }
+	  else{
 		Person.find({},{"_id":0, "id": 1,"name":1},function(err, result){
 		    if (err){
 				res.status(500).send("Error reading movie.");
@@ -114,13 +132,13 @@ router.post('/add', function(req, res, next){
 				return;
 			}
 			var colab = result.filter(x=>unique_people.includes(x.name))
-			
+
 			if (colab.length!==unique_people.length){
 				return res.render("addMovie",{
 					error:"People have to be in the database"
 				});
-			}	
-			
+			}
+
 			let newMovie = new Movie({
 				id: id,
 				title:title,
@@ -158,7 +176,7 @@ router.get('/edit/:id', ensureAuthenticated, function(req, res){
 	}
 	else if (req.user.contributing !== "y"){
 		res.redirect("/users/profile");
-	}	
+	}
 	else{
 	  	Movie.findOne({id:req.params.id}, function(err, movie){
 	  		res.render('editMovie', {
@@ -365,5 +383,218 @@ function ensureAuthenticated(req, res, next){
 	}
 }
 
+function fetchMovieInfo(url){
+  fetch(url)
+    .then(res => res.text())
+    .then(rawHtml => {
+
+      movie = {}
+
+      try{
+        let rawMovieTitle = rawHtml.match('(<div\\sclass="title_wrapper">\\n.+&nbsp;)')[0]
+        rawMovieTitle = rawMovieTitle.substring(27, rawMovieTitle.length);
+        let movieTitleStart = rawMovieTitle.match('(>)')['index'];
+        let movieTitle = rawMovieTitle.substring(movieTitleStart+1, rawMovieTitle.length-6);
+        movie.movieTitle = movieTitle;
+
+        // Unset & delete
+        rawMovieTitle, movieTitleStart, movieTitle = undefined;
+        delete(rawMovieTitle, movieTitleStart, movieTitle);
+
+      }catch {
+        movie.movieTitle = "N/A";
+      };
+
+      // Disgusting regex to find movie release year
+      try{
+        let rawMovieYear = rawHtml.match('(<span\\sid="titleYear">)')['index'];
+        let movieYear = rawHtml.substring(rawMovieYear+60, rawMovieYear+64);
+        movie.movieYear = movieYear;
+
+        // Unset & delete
+        rawMovieYear, movieYear = undefined;
+        delete(rawMovieYear, movieYear);
+
+      }catch {
+        movie.movieYear = "N/A";
+      };
+
+      // Disgusting regex to find age rated
+      try{
+        let rawMovieRating = rawHtml.match('(<time\\sdatetime="PT)')['index'];
+        let movieRating = rawHtml.substring(rawMovieRating-150, rawMovieRating-53);
+        let movieRatingStart = movieRating.match('(\\s{20}.{1,10})')['index'];
+        movieRating = movieRating.substring(movieRatingStart+21, movieRating.length);
+        movie.movieRating = movieRating;
+
+        // Unset & delete
+        rawMovieRating, movieRating, movieRatingStart = undefined;
+        delete(rawMovieRating, movieRating, movieRatingStart);
+
+      }catch {
+        movie.movieRating = "N/A";
+      }
+
+      // Disgusting regex to find runtime
+      try{
+        let rawMovieRuntime = rawHtml.match('([0-9]min)')['index'];
+        let movieRuntimeStart = rawHtml.substring(rawMovieRuntime-50, rawMovieRuntime+20).match('(\\s{24})')['index'];
+        let movieRuntime = rawHtml.substring(rawMovieRuntime-25+movieRuntimeStart, rawMovieRuntime+4);
+        movie.movieRuntime = movieRuntime;
+
+        // Unset & delete
+        rawMovieRuntime, movieRuntimeStart, movieRuntime = undefined;
+        delete(rawMovieRuntime, movieRuntimeStart, movieRuntime);
+
+      }catch {
+        movie.movieRuntime = "N/A";
+      }
+
+      // Disgusting regex to find genre
+      try{
+        let movieGenres = [];
+        let rawMovieGenres = rawHtml.matchAll('(\\/search\\/title\\?genres[^&]+&)');
+        for(const genre of rawMovieGenres){
+          movieGenreEnd = rawHtml.substring(genre['index'], genre['index']+40).match('(&)');
+          movieGenres.push(rawHtml.substring(genre['index']+21, genre['index']+movieGenreEnd['index']));
+        };
+        movieGenres = movieGenres.splice(0, Math.floor(movieGenres.length / 2));
+        movie.movieGenre = movieGenres;
+
+        // Unset & delete
+        movieGenres, rawMovieGenres, movieGenreEnd = undefined;
+        delete(movieGenres, rawMovieGenres, movieGenreEnd);
+
+      }catch {
+        movie.movieGenre = "N/A";
+      }
+
+      // Disgusting regex to find director
+      try{
+        let rawMovieDirector = rawHtml.match('"inline">Director')['index'];
+        let movieDirectorEnd = rawHtml.substring(rawMovieDirector, rawMovieDirector+150).match('<\\/a>')['index'];
+        movie.movieDirector = rawHtml.substring(rawMovieDirector+66, rawMovieDirector+movieDirectorEnd);
+
+        // Unset & delete
+        rawMovieDirector, movieDirectorEnd = undefined;
+        delete(rawMovieDirector, movieDirectorEnd);
+
+      }catch {
+        movie.movieDirector = "N/A";
+      }
+
+      // Disgusting regex to find writers
+      try{
+        let movieWriters = [];
+        let movieWritersStart = rawHtml.match('(Writers)')['index'];
+        let movieWritersEnd = rawHtml.substring(movieWritersStart, movieWritersStart+200).match('(ghost)')['index'];
+        let people = rawHtml.substring(movieWritersStart, movieWritersStart+movieWritersEnd);
+        people = people.matchAll('(tt_ov_wr"\\n>[^<]+)');
+        for(const person of people){
+          movieWriters.push(person[0].substring(11, person[0].length));
+        };
+        movie.movieWriters = movieWriters;
+
+        // Unset & delete
+        movieWriters, movieWritersStart, movieWritersEnd, people = undefined;
+        delete(movieWriters, movieWritersStart, movieWritersEnd, people);
+      }catch {
+        movie.movieWriters = "N/A";
+      }
+
+      // Disgusting regex to find actors
+      try{
+        let movieActors = [];
+        let movieActorsStart = rawHtml.match('(Stars)')['index'];
+        let movieActorsEnd = rawHtml.substring(movieActorsStart, movieActorsStart+400).match('(ghost)')['index'];
+        let people = rawHtml.substring(movieActorsStart, movieActorsStart+movieActorsEnd);
+        people = people.matchAll('(tt_ov_st_sm"\\n>[^<]+)');
+        for(const person of people){
+          movieActors.push(person[0].substring(14, person[0].length));
+        };
+        movie.movieActors = movieActors;
+
+        // Unset & delete
+        movieActors, movieActorsStart, movieActorsEnd, people = undefined;
+        delete(movieActors, movieActorsStart, movieActorsEnd, people);
+
+      }catch {
+        movie.movieActors = "N/A";
+      }
+
+      // Disgusting regex to find plot
+      try {
+        let moviePlotStart = rawHtml.match('(summary_text)')['index'];
+        let moviePlotEnd = rawHtml.substring(moviePlotStart, moviePlotStart+500).match('(<\/div>)')['index'];
+        let moviePlot = rawHtml.substring(moviePlotStart+35, moviePlotStart+moviePlotEnd-13);
+        let cleanedPlot = "";
+        let addLetters = true;
+
+        // Clean out the html tags
+        moviePlot.split('').forEach(c => {
+          if(c == '<'){
+            addLetters = false;
+          };
+          if(addLetters) {
+            cleanedPlot += c;
+          };
+          if(c == '>'){
+            addLetters = true;
+          };
+
+        });
+        movie.moviePlot = cleanedPlot;
+
+        // Unset & delete
+        moviePlotStart, moviePlotEnd, moviePlot, cleanedPlot, addLetters = undefined;
+        delete(moviePlotStart, moviePlotEnd, moviePlot, cleanedPlot, addLetters);
+
+      }catch {
+        movie.moviePlot = "N/A";
+      }
+
+      // Disgusting regex to find poster
+      try{
+        let moviePosterStart = rawHtml.match('(class=\"poster\")')['index'];
+        let moviePosterEnd = rawHtml.substring(moviePosterStart, moviePosterStart+400).match('(\\.jpg)')['index'];
+        let moviePosterStart2 = rawHtml.substring(moviePosterStart, moviePosterStart+moviePosterEnd+4).match('(src=\")')['index'];
+        let moviePoster = rawHtml.substring(moviePosterStart+moviePosterStart2+5, moviePosterStart+moviePosterEnd+4);
+        movie.moviePoster = moviePoster;
+
+        // Unset & delete
+        moviePosterStart, moviePosterEnd, moviePosterStart2, moviePoster = undefined;
+        delete(moviePosterStart, moviePosterEnd, moviePosterStart2, moviePoster);
+
+      }catch {
+        movie.moviePoster = "N/A";
+      }
+
+      // Disgusting regex to find ratings
+      try{
+        let movieMetascoreStart = rawHtml.match('(\"metacriticScore score_favorable titleReviewBarSubItem)')['index'];
+        let movieMetascoreEnd = rawHtml.substring(movieMetascoreStart, movieMetascoreStart+400).match('(</span>)')['index'];
+        let movieMetascore = rawHtml.substring(movieMetascoreStart+63, movieMetascoreStart + movieMetascoreEnd);
+        movie.movieMetascore = movieMetascore
+
+        // Unset & delete
+        movieMetascoreStart, movieMetascoreEnd, movieMetascore = undefined;
+        delete(movieMetascoreStart, movieMetascoreEnd, movieMetascore);
+
+      }catch{
+        movie.movieMetascore = "N/A";
+      }
+
+      // Disgusting regex to find movie rating
+      try {
+        let rawImdbRating = rawHtml.match('(itemprop="ratingValue")')['index'];
+        let movieImdbRating = rawHtml.substring(rawImdbRating+23, rawImdbRating+26);
+        movie.movieImdbRating = movieImdbRating
+      }catch {
+        movie.movieImdbRating = "N/A"
+      }
+
+      console.log(movie);
+    });
+}
 
 module.exports = router;
